e4a3c7664cd937bb4306911dab4295ce6f57c5f8
Brian-kipkoech-mutai <kbrianmutai@gmail.com>
2024-05-28 23:33:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+ 
+
+function App() {
+ 
+
+  return (
+    <>
+    <Button >Click me</Button>
+    <div className="bg-blue-50"></div>
+    </>
+  )
+}
+
+export default App
