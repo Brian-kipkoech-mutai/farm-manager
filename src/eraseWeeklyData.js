73c465f2d8fbfb371319aaa0fc2efc929ecdbfc1
Brian-kipkoech-mutai <kbrianmutai@gmail.com>
2024-06-11 18:07:34 +0300

    correcting data Erasing  algorithim

--- src/eraseWeeklyData.js
+++ src/eraseWeeklyData.js
@@ -1,38 +1,39 @@
-import { getDocument, setDocument } from "./fetch_set_Data";
+ const eraseData = async () => {
+   try {
+     const dataSet = (await getDocument()) || [];
+     const filteredData = dataSet.map((data) => {
+       return {
+         ...data,
+         dailyKilos: {},
+       };
+     });
+     await setDocument(filteredData);
+   } catch (error) {
+     console.warn(`Error in data erasing attempt: ${error}`);
+   }
+ };
 
-const eraseData = async () => {
-  try {
-    const dataSet = (await getDocument()) || [];
-    const filterdData = dataSet.map((data) => {
-      return {
-        ...data,
-        dailyKilos: {},
-      };
-    });
-    await setDocument(filterdData);
-  } catch (error) {
-    console.warn(`error  in data Errasing attempt ${error}`);
-  }
-};
+ const weeklyDataErase = async () => {
+   const currentDate = new Date();
 
-const weeklyDataErase = async () => {
-  const currentDate = new Date();
-  const today = new Date();
-  const daysUntilNextSunday = 7 - today.getDay();
-  today.setDate(today.getDate() + daysUntilNextSunday);
-  today.setHours(23, 59, 59, 999);
-  const nextSunady = new Date(today);
+    
+   const today = new Date();
+   const dayOfWeek = today.getDay();  
+   const daysUntilNextSunday = (7 - dayOfWeek) % 7;
+   today.setDate(today.getDate() + daysUntilNextSunday);
+   today.setHours(23, 59, 59, 999);
+   const nextSunday = new Date(today);
 
-  if (!localStorage.getItem("nextEraseDate")) {
-    localStorage.setItem("nextEraseDate", nextSunady.toISOString());
-  } else {
-    const nextDate = new Date(localStorage.getItem("nextEraseDate"));
-    if (currentDate >= nextDate) {
-      console.log('deleting previous data ' ,{currentDate,nextDate});
-      localStorage.setItem("nextEraseDate", nextSunady.toISOString());
-      await eraseData();
-    }
-  }
-};
+   if (!localStorage.getItem("nextEraseDate")) {
+     localStorage.setItem("nextEraseDate", nextSunday.toISOString());
+   } else {
+     const nextDate = new Date(localStorage.getItem("nextEraseDate"));
+     if (currentDate >= nextDate) {
+       console.log("Deleting previous data", { currentDate, nextDate });
+       localStorage.setItem("nextEraseDate", nextSunday.toISOString());
+       await eraseData();
+     }
+   }
+ };
 
-export default weeklyDataErase;
+ export default weeklyDataErase;
